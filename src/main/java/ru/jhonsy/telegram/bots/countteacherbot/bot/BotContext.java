9c555e02b10c8339d6987fcbd1038ad25d2560c1
Ivan Slastikhin <stopgo89@gmail.com>
2020-07-07 13:55:37 +0300

    Container for bot, user, text is added

---
+++ src/main/java/ru/jhonsy/telegram/bots/countteacherbot/bot/BotContext.java
@@ -0,0 +1,35 @@
+package ru.jhonsy.telegram.bots.countteacherbot.bot;
+
+import ru.jhonsy.telegram.bots.countteacherbot.model.User;
+
+/**
+ * @Author: Ivan Slastikhin
+ */
+public class BotContext {
+
+    private final ChatBot bot;
+    private final User user;
+    private final String input;
+
+    public static BotContext of(ChatBot chatBot, User user, String input){
+        return new BotContext(chatBot, user, input);
+    }
+
+    private BotContext(ChatBot chatBot, User user, String input){
+        this.bot = chatBot;
+        this.user = user;
+        this.input = input;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public ChatBot getBot() {
+        return bot;
+    }
+
+    public String getInput() {
+        return input;
+    }
+}
