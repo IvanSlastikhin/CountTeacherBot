e88ec097670f8607c2f0cbe4477d552162766eb8
Ivan Slastikhin <stopgo89@gmail.com>
2021-07-06 23:14:56 +0300

    Implementated application

--- src/main/java/ru/slastikhin/crud/Application.java
+++ src/main/java/ru/slastikhin/crud/Application.java
@@ -1,7 +1,15 @@
 package ru.slastikhin.crud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author: Ivan Slastikhin
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
